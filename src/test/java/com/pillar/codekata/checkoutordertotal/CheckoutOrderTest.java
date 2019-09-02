9f835136af23b56217583aad8924a3df3cca7790
omergk28 <omergk28@gmail.com>
2019-09-02 11:05:57 -0400

    refactored tests

--- src/test/java/com/pillar/codekata/checkoutordertotal/CheckoutOrderTest.java
+++ src/test/java/com/pillar/codekata/checkoutordertotal/CheckoutOrderTest.java
@@ -1,56 +1,65 @@
 package com.pillar.codekata.checkoutordertotal;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
 
 public class CheckoutOrderTest {
 
+    Item item1;
+    Item item2;
+    Item item3;
+
+
+    @Before
+    public void setup() {
+        initializeTestItems();
+    }
+
+    private void initializeTestItems() {
+        item1 = new Item();
+        item1.setItemName( "foo" );
+        item1.setSoldBy( Item.SOLD_BY.PER_UNIT );
+        item1.setUnitPrice( new BigDecimal( "12.12" ) );
+
+        item2 = new Item();
+        item2.setItemName( "foo" );
+        item2.setUnitPrice( new BigDecimal( "13.13" ) );
+        item2.setSoldBy( Item.SOLD_BY.PER_WEIGHT );
+
+        item3 = new Item();
+        item3.setItemName( "bar" );
+        item3.setUnitPrice( new BigDecimal( "10.00" ) );
+        item3.setSoldBy( Item.SOLD_BY.PER_WEIGHT );
+    }
 
     @Test
     public void addItemToOrderShouldCreateALineItemAndAddItToOrderItemsAndUpdateTheItemOrderTotalAmount() {
         CheckoutOrder checkoutOrder = new CheckoutOrder();
 
-        Item testItem = new Item();
+        checkoutOrder.addItemToInventory( item1 );
 
-        testItem.setItemName( "foo" );
-        testItem.setSoldBy( Item.SOLD_BY.PER_UNIT );
-        testItem.setUnitPrice( new BigDecimal( "12.12" ) );
-
-        checkoutOrder.addItemToInventory( testItem );
-
-        LineItem addedLineItem = checkoutOrder.addItemToOrder( testItem, new BigDecimal( "1" ) );
+        LineItem addedLineItem = checkoutOrder.addItemToOrder( item1, new BigDecimal( "1" ) );
 
         Assert.assertTrue( checkoutOrder.getOrderItems().containsKey( addedLineItem.getLineItemId() ) );
         Assert.assertEquals( new BigDecimal( "1" ), checkoutOrder.getOrderItemTotalAmount()
-                .get( testItem.getItemName() ) );
+                .get( item1.getItemName() ) );
     }
 
     @Test
     public void addItemToInventoryShouldUpdatePreviouslyDefinedItem() {
         CheckoutOrder checkoutOrder = new CheckoutOrder();
 
-        Item testItem = new Item();
-
-        testItem.setItemName( "foo" );
-        testItem.setSoldBy( Item.SOLD_BY.PER_UNIT );
-        testItem.setUnitPrice( new BigDecimal( "12.12" ) );
-
-        checkoutOrder.addItemToInventory( testItem );
+        checkoutOrder.addItemToInventory( item1 );
 
         Assert.assertTrue( checkoutOrder.getInventory().containsKey( "foo" ) );
         Assert.assertEquals( "foo", checkoutOrder.getInventory().get( "foo" ).getItemName() );
         Assert.assertEquals( Item.SOLD_BY.PER_UNIT, checkoutOrder.getInventory().get( "foo" ).getSoldBy() );
         Assert.assertEquals( new BigDecimal( "12.12" ), checkoutOrder.getInventory().get( "foo" ).getUnitPrice() );
 
-        Item replacementItem = new Item();
-
-        replacementItem.setItemName( "foo" );
-        replacementItem.setUnitPrice( new BigDecimal( "13.13" ) );
-        replacementItem.setSoldBy( Item.SOLD_BY.PER_WEIGHT );
-
-        checkoutOrder.addItemToInventory( replacementItem );
+        checkoutOrder.addItemToInventory( item2 );
 
         Assert.assertEquals( "foo", checkoutOrder.getInventory().get( "foo" ).getItemName() );
         Assert.assertEquals( Item.SOLD_BY.PER_WEIGHT, checkoutOrder.getInventory().get( "foo" ).getSoldBy() );
@@ -61,13 +70,7 @@ public class CheckoutOrderTest {
     public void addItemToInventoryShouldAddItemToInventory() throws Exception {
         CheckoutOrder checkoutOrder = new CheckoutOrder();
 
-        Item testItem = new Item();
-
-        testItem.setItemName( "foo" );
-        testItem.setSoldBy( Item.SOLD_BY.PER_UNIT );
-        testItem.setUnitPrice( new BigDecimal( "12.12" ) );
-
-        checkoutOrder.addItemToInventory( testItem );
+        checkoutOrder.addItemToInventory( item1 );
 
         Assert.assertTrue( checkoutOrder.getInventory().containsKey( "foo" ) );
         Assert.assertEquals( "foo", checkoutOrder.getInventory().get( "foo" ).getItemName() );
